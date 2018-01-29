bf2f093ca550daf1aed267f9087e2c95d4801c95
Cassidy <cmdulemba@oakland.edu>
2018-01-29 16:38:05 -0500

    Functionality added to BeerSong

--- BeerSong.java
+++ BeerSong.java
@@ -1,7 +1,25 @@
-public static void main(String [] args) {
+public class BeerBottle {
 
-}
+	public static void main(String [] args) {
+		BeerBottle.Ninety_Nine_Bottles_of_Beer(); //calling the function
 
-Ninety_Nine_Bottles_of_Beer() {
+	}
+
+
+	static void Ninety_Nine_Bottles_of_Beer() {
+		String s = "s";
+		for (int beers=99; beers>-1; beers--)
+		{
+			System.out.print(beers + " bottle" + s + " of beer on the wall, ");
+			System.out.println(beers + " bottle" + s + " of beer, ");
+	
+			System.out.print("Take one down, pass it around, ");
+			s = (--beers ==1)?"":"s";
+			System.out.println(beers + " bottle" + s + " of beer on the wall.\n");
+		}
+		/**
+
+		*/
+	}
 
 }
